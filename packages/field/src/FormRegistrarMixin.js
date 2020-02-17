9bde4fa7724e4da017ac280bc41d2f1d2899772e
Thijs Louisse <Thijs.Louisse@ing.com>
2020-02-17 13:06:04 +0100

    wip: diff

--- packages/field/src/FormRegistrarMixin.js
+++ packages/field/src/FormRegistrarMixin.js
@@ -1,29 +1,46 @@
+// eslint-disable-next-line max-classes-per-file
 import { dedupeMixin } from '@lion/core';
 import { FormRegisteringMixin } from './FormRegisteringMixin.js';
 import { formRegistrarManager } from './formRegistrarManager.js';
+import { FormControlsCollection } from './FormControlsCollection.js';
+
+// TODO: rename .formElements to .formControls? (or .$controls ?)
 
 /**
- * This allows an element to become the manager of a register
+ * @desc This allows an element to become the manager of a register.
+ * It basically keeps track of a FormControlsCollection that it stores in .formElements
+ * This will always be an array of all elements.
+ * In case of a form or fieldset(sub form), it will also act as a key based object with FormControl
+ * (fields, choice groups or fieldsets)as keys.
+ * For choice groups, the value will only stay an array.
+ * See FormControlsCollection for more information
  */
 export const FormRegistrarMixin = dedupeMixin(
   superclass =>
     // eslint-disable-next-line no-shadow, no-unused-vars
     class FormRegistrarMixin extends FormRegisteringMixin(superclass) {
-      get formElements() {
-        return this.__formElements;
-      }
-
-      set formElements(value) {
-        this.__formElements = value;
-      }
-
-      get formElementsArray() {
-        return this.__formElements;
+      static get properties() {
+        return {
+          /**
+           * @desc Flag that determines how ".formElements" should behave.
+           * For a regular fieldset (see LionFieldset) we expect ".formElements"
+           * to be accessible as an object.
+           * In case of a radio-group, a checkbox-group or a select/listbox,
+           * it should act like an array (see ChoiceGroupMixin).
+           * Usually, when false, we deal with a choice-group (radio-group, checkbox-group,
+           * (multi)select)
+           * @type {boolean}
+           */
+          _isFormOrFieldset: Boolean,
+        };
       }
 
       constructor() {
         super();
-        this.formElements = [];
+        this.formElements = new FormControlsCollection();
+
+        this._isFormOrFieldset = false;
+
         this.__readyForRegistration = false;
         this.__hasBeenRendered = false;
         this.registrationReady = new Promise(resolve => {
@@ -52,7 +69,7 @@ export const FormRegistrarMixin = dedupeMixin(
       }
 
       isRegisteredFormElement(el) {
-        return this.formElementsArray.some(exitingEl => exitingEl === el);
+        return this.formElements.some(exitingEl => exitingEl === el);
       }
 
       firstUpdated(changedProperties) {
@@ -63,22 +80,68 @@ export const FormRegistrarMixin = dedupeMixin(
         this.__hasBeenRendered = true;
       }
 
-      addFormElement(child, index) {
+      addFormElement(child, indexToInsertAt) {
         // This is a way to let the child element (a lion-fieldset or lion-field) know, about its parent
         // eslint-disable-next-line no-param-reassign
         child.__parentFormGroup = this;
 
-        if (index > 0) {
-          this.formElements.splice(index, 0, child);
+        // 1. Add children as array element
+        if (indexToInsertAt > 0) {
+          this.formElements.splice(indexToInsertAt, 0, child);
         } else {
           this.formElements.push(child);
+        }
+
+        // 2. Add children as object key
+        if (this._isFormOrFieldset) {
+          const { name } = child;
+          if (!name) {
+            console.info('Error Node:', child); // eslint-disable-line no-console
+            throw new TypeError('You need to define a name');
+          }
+          if (name === this.name) {
+            console.info('Error Node:', child); // eslint-disable-line no-console
+            throw new TypeError(`You can not have the same name "${name}" as your parent`);
+          }
+
+          if (name.substr(-2) === '[]') {
+            if (!Array.isArray(this.formElements[name])) {
+              this.formElements[name] = [];
+            }
+            if (indexToInsertAt > 0) {
+              this.formElements[name].splice(indexToInsertAt, 0, child);
+            } else {
+              this.formElements[name].push(child);
+            }
+          } else if (!this.formElements[name]) {
+            this.formElements[name] = child;
+          } else {
+            console.info('Error Node:', child); // eslint-disable-line no-console
+            throw new TypeError(
+              `Name "${name}" is already registered - if you want an array add [] to the end`,
+            );
+          }
         }
       }
 
       removeFormElement(child) {
+        // 1. Handle array based children
         const index = this.formElements.indexOf(child);
         if (index > -1) {
           this.formElements.splice(index, 1);
+        }
+
+        // 2. Handle name based object keys
+        if (this._isFormOrFieldset) {
+          const { name } = child;
+          if (name.substr(-2) === '[]' && this.formElements[name]) {
+            const idx = this.formElements[name].indexOf(child);
+            if (idx > -1) {
+              this.formElements[name].splice(idx, 1);
+            }
+          } else if (this.formElements[name]) {
+            delete this.formElements[name];
+          }
         }
       }
 
@@ -106,11 +169,11 @@ export const FormRegistrarMixin = dedupeMixin(
       _onRequestToRemoveFormElement(ev) {
         const child = ev.detail.element;
         if (child === this) {
-          // as we fire and listen - don't add ourselves
+          // as we fire and listen - don't remove ourselves
           return;
         }
         if (!this.isRegisteredFormElement(child)) {
-          // do not readd already existing elements
+          // do not remove non existing elements
           return;
         }
         ev.stopPropagation();
